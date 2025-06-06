0ad25fc640310ec5b032e8ba0d14cbfaf8c2ff74
Maria Luisa M <168206693+MariaLuisaR2024@users.noreply.github.com>
2025-06-05 23:45:01 -0300

    h
    h

---
+++ flashcards.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const container = document.getElementById("flashcard-container");
+
+  fetch("../data/biologia.json")
+    .then(res => res.json())
+    .then(data => {
+      data.forEach(card => {
+        const div = document.createElement("div");
+        div.className = "flashcard";
+        div.innerHTML = `
+          <div class="front">${card.pergunta}</div>
+          <div class="back">${card.resposta}</div>
+        `;
+        container.appendChild(div);
+      });
+    });
+});
